9490515584444c214703f1a683b782e6bba1f1a9
evanlaws <58115202+evanlaws@users.noreply.github.com>
2024-11-01 11:50:59 -0700

    Add files via upload

---
+++ new-game-project/scripts/player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+
+const SPEED = 130.0
+var jump_velocity = -600.0
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+	
+	if Input.is_action_just_pressed("jump") and velocity.y < 0:
+		velocity.y *= 0.5
+		
+
+	# get the input direction
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	# play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
